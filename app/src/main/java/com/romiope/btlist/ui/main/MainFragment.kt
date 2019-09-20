a2e6be4d83d8487c672fa458236a6d06b0e1ecb6
Roman Zaiarnyi <Roman.Zaiarnyi@soti.net>
2019-09-20 18:15:11 -0400

    Added RecyclerView with list of paired devices

--- app/src/main/java/com/romiope/btlist/ui/main/MainFragment.kt
+++ app/src/main/java/com/romiope/btlist/ui/main/MainFragment.kt
@@ -1,12 +1,16 @@
 package com.romiope.btlist.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.romiope.btlist.R
+import kotlinx.android.synthetic.main.list_item.view.*
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
@@ -26,7 +30,28 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        val devices = viewModel.getDevices()
-        devices.forEach { System.err.println("MainFragment: connected BT device names ${it.name}") }
+
+        list.apply {
+            layoutManager = LinearLayoutManager(context)
+
+            adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+                val devices = viewModel.getDevices().toList()
+
+                override fun onCreateViewHolder(
+                    parent: ViewGroup,
+                    viewType: Int
+                ) = object: RecyclerView.ViewHolder(
+                    LayoutInflater.from(context).inflate(R.layout.list_item, null)
+                ) {}
+
+                override fun getItemCount() = devices.size
+
+                override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+                    holder.itemView.name.text = devices[position].name
+                    holder.itemView.deviceId.text = devices[position].address
+                }
+            }
+        }
     }
 }
